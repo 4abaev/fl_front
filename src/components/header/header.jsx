0cf03a2df8ca930a064b7f1e08a972820b8ea3bb
4abaev <progchab@gmail.com>
2023-06-27 19:37:42 +0300

    правки заказчика

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,85 @@
-import { useState } from "react";
 import styles from "./header.module.scss";
-import Modal from "../modal/modal";
-import Create from "../createUser/create";
+import { createPeople } from "../../state/actions";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { Input } from "../input/input";
 
 const Header = () => {
-    const [open, setOpen] = useState();
+    function generateRandomEmail() {
+        const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+        let email = "";
+
+        // Generate a random string of 10 characters
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            email += characters[randomIndex];
+        }
+
+        // Add the domain part of the email
+        email += "@example.com";
+
+        return email;
+    }
+    function generateRandomPassword(length) {
+        const characters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+        let password = "";
+
+        // Generate a random string of specified length
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            password += characters[randomIndex];
+        }
+
+        return password;
+    }
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+        reset
+    } = useForm({ mode: "onChange" });
+
+    const dispatch = useDispatch();
+    const onSubmit = (data) => {
+        dispatch(
+            createPeople({
+                name: data.Name,
+                email: generateRandomEmail(),
+                password: generateRandomPassword(10),
+            })
+        );
+        reset()
+    };
     return (
         <div className={styles.wrapper}>
             <h1>Панель управления</h1>
             <div className={styles.hr}></div>
-            <button onClick={() => setOpen(true)}>Создать новую персону</button>
-            {open && (
-                <Modal
-                    closeModal={() => setOpen(false)}
-                    children={<Create closeModal={() => setOpen(false)} />}
-                />
-            )}
+            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <Input
+                type="text"
+                name="Name"
+                label="Имя персоны"
+                errors={errors}
+                register={register("Name", {
+                    required: "Это поле обязательно для заполнения",
+                    minLength: {
+                        value: 3,
+                        message: "Имя пользователя слишком короткое",
+                    },
+                    maxLength: {
+                        value: 28,
+                        message: "Имя пользователя слишком длинное",
+                    },
+                    pattern: {
+                        value: /^[a-zа-яё0-9]+$/i,
+                        message: "Только буквы и числа",
+                    },
+                })}
+            />
+            <button className={styles.button} type="submit">Сохранить</button>
+        </form>
+
         </div>
     );
 };
--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,43 +1,62 @@
 import { useState } from "react";
 import styles from "./user.module.scss";
-import Modal from "../modal/modal";
-import Delete from "../deleteUser/delete";
-import Edit from "../editUser/edit";
+import { useDispatch } from "react-redux";
+import { deletePeople, patchPeople } from "../../state/actions";
+import { useForm } from "react-hook-form";
+import { Input } from "../input/input";
 
 const User = ({ props }) => {
-    const [deleteOpen, setDeleteOpen] = useState(false);
     const [changeOpen, setChangeOpen] = useState(false);
 
-    return (
+    const dispatch = useDispatch();
+    const onSubmit = (data) => {
+        dispatch(patchPeople({ id: props.id, name: data.Name }));
+        setChangeOpen(false);
+    };
+
+    const onDelete = () => {
+        dispatch(deletePeople({ id: props.id }));
+    };
+
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm({ mode: "onChange" });
+    return changeOpen ? (
+        <form className={styles.user__wrapper} onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.id}>{props.id}</div>
+            <Input
+                type="text"
+                name="Name"
+                label="Имя персоны"
+                errors={errors}
+                register={register("Name", {
+                    required: "Это поле обязательно для заполнения",
+                    minLength: {
+                        value: 3,
+                        message: "Имя пользователя слишком короткое",
+                    },
+                    maxLength: {
+                        value: 28,
+                        message: "Имя пользователя слишком длинное",
+                    },
+                    pattern: {
+                        value: /^[a-zа-яё0-9]+$/i,
+                        message: "Только буквы и числа",
+                    },
+                })}
+            />
+            <button className={styles.button} type="submit">Сохранить</button>
+        </form>
+    ) : (
         <div className={styles.user__wrapper}>
             <div className={styles.id}>{props.id}</div>
             <div className={styles.name}>{props.name}</div>
             <div className={styles.buttons}>
                 <button onClick={() => setChangeOpen(true)}>&#9998;</button>
-                <button onClick={() => setDeleteOpen(true)}>&#10060;</button>
+                <button onClick={onDelete}>&#10060;</button>
             </div>
-            {deleteOpen && (
-                <Modal
-                    closeModal={() => setDeleteOpen(false)}
-                    children={
-                        <Delete
-                            props={props}
-                            closeModal={() => setDeleteOpen(false)}
-                        />
-                    }
-                />
-            )}
-            {changeOpen && (
-                <Modal
-                    closeModal={() => setChangeOpen(false)}
-                    children={
-                        <Edit
-                            closeModal={() => setChangeOpen(false)}
-                            props={props}
-                        />
-                    }
-                />
-            )}
         </div>
     );
 };
